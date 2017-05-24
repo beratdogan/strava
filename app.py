7785348814d372777767a5ffa71502599dce62fc
Berat Dogan <beratdgan@gmail.com>
2017-05-24 23:53:39 +0300

    Implemented endpoint 2 Fetches most popular segments and their leaders, calculates a score by oredefined rules

--- app.py
+++ app.py
@@ -49,24 +49,96 @@ def get_leaders(location):
             if entry['athlete_id'] not in athletes:
                 athletes[entry['athlete_id']] = {
                     'athlete_name': entry['athlete_name'],
-                    'athlete_gender': entry['athlete_gender'],
-                    'distance': entry['distance'],
-                    'moving_time': entry['moving_time'],
-                    'elapsed_time': entry['elapsed_time'],
+                    'total_distance': 0,
+                    'total_moving_time': 0,
+                    'total_elapsed_time': 0,
                     'avg_speed': entry['distance'] / entry['moving_time'],
                     'count': 0,
                 }
-            athletes[entry['athlete_id']]['count'] += 1
 
-    return {k: v for k, v in athletes.iteritems() if v['count'] > 1}
+            athletes[entry['athlete_id']]['count'] += 1
+            athletes[entry['athlete_id']]['total_distance'] += entry['distance']
+            athletes[entry['athlete_id']]['total_moving_time'] += entry['moving_time']
+            athletes[entry['athlete_id']]['total_elapsed_time'] += entry['elapsed_time']
+
+    return athletes
+
+
+def calculate_score(athlete):
+    """
+    There are simple rules of calculating scores as below:
+    - immovability: Riders who don't take too many breaks while riding earns
+                    extra points. Since this doesn't have a big impact,
+                    the coefficient is very low.
+    - distance: This displays the total distance traveled within the given
+                    boundaries. Passing a certain threshold earns
+                    extra points. Since long distance bike riding is important,
+                    the coefficient is higher.
+    - avg speed: This displays the average riding speed for all the distance
+                    traveled within the given boundaries. An average speed is
+                    considered better. The riders who are not too slow or
+                    too fast earns extra points.
+    - count: This displays the number of different segments the rider have
+                    traveled on within the given boundaries. This number
+                    signifies how "continuous" the rider is. Because of this,
+                    the parameter has a very high coefficient.
+    After the values of these parameters are calculated, they are multiplied
+                    with their coefficients and the total sum is calculated.
+    """
+    immovability = abs(athlete['total_elapsed_time'] - athlete['total_moving_time'])
+    if immovability > 10:
+        immovability_score = 0
+    else:
+        immovability_score = 10
+
+    distance = athlete['total_distance']
+    distance_cluster = divmod(distance, 50)[0]
+    distance_score = distance_cluster * 10
+
+    avg_speed = athlete['avg_speed']
+    if 15 >= avg_speed >= 10:
+        avg_speed_bonus = 10
+    else:
+        avg_speed_bonus = -10
+
+    count = athlete['count']
+    count_bonus = 0
+    if count >= 7:
+        count_bonus = 10
+    elif count >= 3:
+        count_bonus = 5
+
+    return 1 * immovability_score + 4 * distance_score + 2 * avg_speed_bonus + 5 * count_bonus
+
+
+def normalize_scores(athletes):
+    sum_scores = sum(athlete['score'] for athlete_id, athlete in athletes.iteritems())
+
+    for athlete_id, athlete in athletes.iteritems():
+        athlete['score'] = athlete['score'] / sum_scores
 
 
 @app.route('/', defaults={'location': 'istanbul'})
 @app.route('/<string:location>')
 def active_atheletes(location):
-    leaders = get_leaders(location.lower())
+    athletes = get_leaders(location.lower())
 
-    return jsonify(leaders)
+    filtered_athletes = {k: v for k, v in athletes.iteritems() if v['count'] > 1}
+
+    return jsonify(filtered_athletes)
+
+
+@app.route('/highscores', defaults={'location': 'istanbul'})
+@app.route('/highscores/<string:location>')
+def high_scores(location):
+    athletes = get_leaders(location.lower())
+
+    for athlete_id, athlete in athletes.iteritems():
+        athlete['score'] = calculate_score(athlete)
+
+    normalize_scores(athletes)
+
+    return jsonify(athletes)
 
 
 if __name__ == '__main__':
